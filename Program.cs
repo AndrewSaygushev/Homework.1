c126b0ce2b8ee1e6c33260368735b253076c7036
Andrew <andrew6594@gmail.com>
2022-11-23 10:23:59 +0300

    added task2

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int Number2 = Convert.ToInt32(Console.ReadLine());
+
+if (Number1 > Number2)
+{
+Console.WriteLine($"Первое число {Number1} больше, чем второе число {Number2}");
+
+}
+else
+{
+Console.WriteLine($"Второе число {Number2} больше, чем первое число {Number1}");
+}
+
+Console.WriteLine();
